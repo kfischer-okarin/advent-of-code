2708fb87e3adb49e972c79a327ebdbb24d30f215
Kevin Fischer <kfischer_okarin@yahoo.co.jp>
2020-01-16 09:24:39 +0900

    Add task template

---
+++ lib/task_template.rb
@@ -0,0 +1,12 @@
+require_relative 'common'
+
+module TaskXX
+
+
+  if $PROGRAM_NAME == __FILE__
+    # data = read_input_lines('xx')
+
+    # puts "1) Solution 1: #{}"
+    # puts "2) Solution 2: #{}"
+  end
+end
