aea71fc5ad09ae4a8292fff55e11a9204d489814
Kevin Fischer <kfischer_okarin@yahoo.co.jp>
2021-12-04 18:33:48 +0900

    Extract button class

--- 2021/game/app/menu.rb
+++ 2021/game/app/menu.rb
@@ -1,3 +1,5 @@
+require 'lib/button.rb'
+
 class Menu
   def initialize(args)
     args.state.buttons = SOLUTIONS.keys.sort!.map { |number| build_button(number) }
@@ -6,8 +8,7 @@ class Menu
   def tick(args)
     buttons = args.state.buttons
     render_buttons(args.outputs, buttons)
-    reset_hover(buttons)
-    handle_mouse(args, buttons)
+    handle_buttons(args, buttons)
   end
 
   private
@@ -15,47 +16,24 @@ class Menu
   def build_button(number)
     title = SOLUTIONS[number].title
     width = $gtk.calcstringbox(title)[0] + 20
-    {
-      number: number,
-      label: title,
+    Button.new(
+      id: number,
       rect: { x: 640 - width.idiv(2), y: 720 - (number * 60), w: width, h: 40 },
-      hover: false
-    }
+      label: title,
+      click_handler: ->(args, _) { start_solution(args, number) }
+    )
   end
 
   def render_buttons(gtk_outputs, buttons)
-    gtk_outputs.primitives << buttons.map { |button|
-      button_primitives(
-        rect: button[:rect],
-        label: button[:label],
-        inverted: button[:hover]
-      )
-    }
+    buttons.each do |button|
+      button.render(gtk_outputs)
+    end
   end
 
-  def button_primitives(rect:, label:, inverted:)
-    button_background = inverted ? rect.to_solid : rect.to_border
-    label_color = inverted ? { r: 255, g: 255, b: 255 } : { r: 0, g: 0, b: 0 }
-    [
-      button_background,
-      { x: rect.x + rect.w.idiv(2), y: rect.y + 30, text: label, alignment_enum: 1 }.label!(label_color)
-    ]
-  end
-
-  def reset_hover(buttons)
-    buttons.each { |button| button[:hover] = false }
-  end
-
-  def handle_mouse(args, buttons)
-    mouse = args.inputs.mouse
-
-    hover_button = buttons.find { |button| mouse.inside_rect? button[:rect] }
-    return unless hover_button
-
-    hover_button[:hover] = true
-    return unless mouse.down
-
-    start_solution(args, hover_button[:number])
+  def handle_buttons(args, buttons)
+    buttons.each do |button|
+      button.tick(args)
+    end
   end
 
   def start_solution(args, number)
---
+++ 2021/game/lib/button.rb
@@ -0,0 +1,43 @@
+class Button
+  attr_accessor :id, :rect, :label, :click_handler
+
+  def initialize(id:, rect:, label:, click_handler: nil)
+    @id = id
+    @rect = rect
+    @label = label
+    @hover = false
+    @click_handler = click_handler || ->(_args, _button) {}
+  end
+
+  def tick(args)
+    mouse = args.inputs.mouse
+    @hover = mouse.inside_rect? @rect
+    return unless @hover && mouse.down
+
+    @click_handler.call(args, self)
+  end
+
+  def render(gtk_outputs)
+    gtk_outputs.primitives << [
+      button_background,
+      {
+        x: @rect.x + @rect.w.idiv(2), y: @rect.y + @rect.h.idiv(2),
+        text: @label, alignment_enum: 1, vertical_alignment_enum: 1
+      }.label!(label_color)
+    ]
+  end
+
+  def to_s
+    { id: @id, rect: @rect, label: @label }.to_s
+  end
+
+  private
+
+  def button_background
+    @hover ? @rect.to_solid : @rect.to_border
+  end
+
+  def label_color
+    @hover ? { r: 255, g: 255, b: 255 } : { r: 0, g: 0, b: 0 }
+  end
+end
